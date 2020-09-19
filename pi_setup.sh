bd29a5f494b616a9f2f52b92b4a2ebb86778b713
Nic Roberts II <nrober@umich.edu>
2020-09-19 15:28:42 -0400

    update conda install commands to the ones that actually worked

--- pi_setup.sh
+++ pi_setup.sh
@@ -9,24 +9,19 @@ sudo apt-get -y upgrade
 #--------------
 #Install conda
 #--------------
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh
-sudo /bin/bash Miniconda3-latest-Linux-armv7l.sh
-echo 'export PATH="home/pi/miniconda3/bin:$PATH"' | sudo tee -a /home/pi/.bashrc
+curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-armv7l.sh" -o "Miniconda.sh"
+bash ./Miniconda.sh
+echo "yes yes" | ./Miniconda.sh
 
 #--------------
 #Install docker
 #--------------
 sudo apt-get -y install apt-transport-https ca-certificates software-properties-common -y
-
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
-
 sudo usermod -aG docker pi
-
 echo 'deb https://download.docker.com/linux/raspbian/ stretch stable' | sudo tee -a /etc/apt/sources.list
-
 sudo apt-get -y update
 sudo apt-get -y upgrade
-
 systemctcl start docker.service
 
 #--------------
