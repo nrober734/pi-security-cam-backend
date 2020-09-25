dc24552636758729a9b3ddbb3282329a1f86eb92
Nic Roberts II <nic2@Nics-MBP.lan>
2020-09-24 21:31:51 -0400

    pr comments fixed for camera_runtime

--- camera_runtime.py
+++ camera_runtime.py
@@ -3,14 +3,13 @@ from yolo_inference import YoloExecutor
 from stream_processor import StreamProcessor
 
 
-class CameraRuntime:
+class CameraProcessingModule:
 
     def __init__(self):
-
         self.yolo = YoloExecutor()
         self.stream_processor = StreamProcessor(operation='add')
 
-    def fetch_inventory(self) -> dict:
+    def get_detections(self) -> dict:
         #   Launches the intelligent inventory collection
         self.stream_processor.process_stream()
         input_frame = Image.open('add_image.jpg')
--- run_security_cam.py
+++ run_security_cam.py
@@ -1,14 +1,14 @@
 from inventory_state_utils import update_inventory_state
-from camera_runtime import CameraRuntime
+from camera_runtime import CameraProcessingModule
 
 
 def camera_detection_runtime() -> None:
 
     inventory_state_dict = {}
-    camera_runtime = CameraRuntime()
+    camera_runtime = CameraProcessingModule()
 
     while True:
-        add_objects = camera_runtime.fetch_inventory()
+        add_objects = camera_runtime.get_detections()
         object_detection_state_dict = update_inventory_state(input_items=add_objects,
                                                              state_inventory_dict=inventory_state_dict,
                                                              operation='add')
